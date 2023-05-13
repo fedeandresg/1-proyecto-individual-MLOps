a18db57be7cfa108476b01e685188f7d90debed5
fedeandresg <cpfedericogomez@gmail.com>
2023-05-13 12:06:45 -0300

    second commit

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 # Importamos las librerías
 import pandas as pd 
 import numpy as np
+import sklearn
 from fastapi import FastAPI
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import HashingVectorizer
 
 # Indicamos título y descripción de la API
 app = FastAPI(title='PROYECTO INDIVIDUAL Nº1 -Machine Learning Operations (MLOps)',
             description='API de datos y recomendaciones de películas')
 
-# http://127.0.0.1:8000
+# Datasets
+df = pd.read_csv('movies_final.csv')
+df1 = pd.read_csv('movies_ml.csv')
 
-
-# Función para que el la API tome el dataframe
-#@app.get('/')
-#async def read_root():
-#    return {'Hola! Bienvenido a la API de recomedación. Por favor dirigite a /docs'}
-    
-@app.on_event('startup')
-async def startup():
-    global df
-    df = pd.read_csv('movies_final.csv') 
 
 # Función para reconocer el servidor local
 
@@ -70,6 +65,7 @@ def peliculas_mes(mes:str):
 
     return {'mes':mes, 'cantidad':respuesta}
 
+
 # Función de películas por día
 
 @app.get('/peliculas_dia/({dia})')
@@ -111,6 +107,7 @@ def franquicia(franquicia:str):
 
     return {'franquicia':franquicia, 'cantidad':cantidad_peliculas_franq, 'ganancia_total':revenue_franq, 'ganancia_promedio':promedio_franq}
 
+
 # Función películas por país
 
 @app.get('/peliculas_pais/({pais})')
@@ -140,6 +137,7 @@ def productoras(productora:str):
 
     return {'productora':productora, 'ganancia_total':revenue_prod, 'cantidad':cantidad_peliculas_prod}
 
+
 # Función métricas por película
 
 @app.get('/retorno/({pelicula})')
@@ -157,3 +155,60 @@ def retorno(pelicula):
     return {'pelicula':pelicula_nombre, 'inversion':inversion_pelicula, 'ganacia':ganancia_pelicula,'retorno':retorno_pelicula, 'anio':year_pelicula}
 
 
+# Función de recomendación
+
+# Aseguramos que los datos de la columna 'overview' sean strings
+df1['overview'] = df1['overview'].fillna('').astype('str')
+
+# Aseguramos que los datos de la columna 'genres' sean strings
+df1['genres'] = df1['genres'].apply(lambda x: ' '.join(map(str, x)) if isinstance(x, list) else '')
+
+# Reemplazar los valores NaN con cadenas vacías en la columna 'production_companies'
+df1['production_companies'] = df1['production_companies'].fillna('')
+
+# Convertir la columna 'production_companies' a string si es necesario
+df1['production_companies'] = df1['production_companies'].apply(lambda x: ' '.join(map(str, x)) if isinstance(x, list) else x)
+
+# Crear una nueva columna combinando las características de interés
+df1['combined_features'] = df1['overview'] + ' ' + df1['genres'] + ' ' + df1['production_companies']
+
+# Convertimos todos los textos a minusculas para evitar duplicados
+df1['combined_features'] = df1['combined_features'].str.lower()
+
+# Inicializamos el HashingVectorizer
+hash_vectorizer = HashingVectorizer(stop_words='english', n_features=2000)
+
+# Transformamos los datos
+hash_matrix = hash_vectorizer.fit_transform(df1['combined_features'])
+
+# Calculamos la similitud del coseno
+cosine_sim = cosine_similarity(hash_matrix)
+
+# Creamos un índice con los títulos de las películas
+indices = pd.Series(df1.index, index=df1['title']).drop_duplicates()
+
+
+@app.get("/recomendacion/{titulo}")
+def recomendacion(titulo: str):
+    '''Ingresas un nombre de pelicula y te recomienda 5 similares
+    '''
+    if titulo not in df1['title'].values:
+        return {'message': 'La película no se encuentra en el conjunto de datos de muestra.'}
+    else:
+        # Obtenemos el índice de la película que coincide con el título
+        idx = indices[titulo]
+
+        # Obtenemos las puntuaciones de similitud de todas las películas con la película dada
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        # Ordenamos las películas en función de las puntuaciones de similitud
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Obtenemos las puntuaciones de las 5 películas más similares
+        sim_scores = sim_scores[1:6]
+
+        # Obtenemos los índices de las películas
+        movie_indices = [i[0] for i in sim_scores]
+
+        # Devolvemos las 5 películas más similares
+        return {'recomendaciones': list(df1['title'].iloc[movie_indices])}
